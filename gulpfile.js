efb136b68f1e6fea95ddf6799dc1e708ef702b86
Tatsiana <t_a_b@tut.by>
2019-11-26 09:45:42 +0300

    corrected task copy in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -72,7 +72,6 @@ gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff, woff2}",
       "source/img/**",
-      "source/js/**"
       ], {
       base: "source"
     })
