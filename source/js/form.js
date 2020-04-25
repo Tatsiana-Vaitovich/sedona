471acae42a4a81f05f52f80d960804ff8d97dff2
Tatsiana <t_a_b@tut.by>
2020-04-26 01:19:47 +0300

    add form.js

--- source/js/form.js
+++ source/js/form.js
@@ -3,5 +3,11 @@
 const form = document.querySelector(".comment__form");
 
 form.addEventListener("submit", function(evt) {
-  console.log(evt);
+  console.log("work")
+  const elements = form.elements;
+  for (let i =0; i < elements.length; i++) {
+    if(!elements[i].validity.valid) {
+      elements[i].style.backgroundColor = "red";
+    }
+  }
 })
