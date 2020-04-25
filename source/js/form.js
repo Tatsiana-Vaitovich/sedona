8ddf921519de232639eefa2ad15361455b09af30
Tatsiana <t_a_b@tut.by>
2020-04-26 00:15:36 +0300

    add event resize for window

---
+++ source/js/form.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const form = document.querySelector(".comment__form");
+
+form.addEventListener("submit", function(evt) {
+  console.log(evt);
+})
--- source/js/main.js
+++ source/js/main.js
@@ -5,17 +5,35 @@ const mainMenu = document.querySelector(".main-menu");
 const btnMenuOpen = mainNavigation.querySelector(".main-header__btn--open");
 const btnMenuClose = mainNavigation.querySelector(".main-header__btn--close")
 
+// исключу вариант сценария, когда меню будет закрыто
+// при увеличении ширины окна от mobile до tablet:
+window.addEventListener("resize", (evt) => {
+  const screenWidth = document.documentElement.scrollWidth;
+  const screenTablet = 768;
+  if (screenWidth >= screenTablet) {
+    showElem(mainMenu);
+    changeDisplay(btnMenuOpen, "none");
+    changeDisplay(btnMenuClose, "none")
+  } else if (screenWidth <= screenTablet) {
+    changeDisplay(btnMenuClose, "block");
+  }
+})
+
 mainNavigation.addEventListener("click", function(evt) {
   const elem = evt.target;
   if (elem.closest(".main-header__btn--open")) {
-    showElem(btnMenuClose);
+    // showElem(btnMenuClose);
+    changeDisplay(btnMenuClose, "block");
     showElem(mainMenu);
-    btnMenuOpen.style.visibility = "hidden";
+    // btnMenuOpen.style.visibility = "hidden";
+    changeDisplay(btnMenuOpen, "none");
   } 
   else if (elem.closest(".main-header__btn--close")) {
-    hiddenElem(btnMenuClose);
+    // hiddenElem(btnMenuClose);
+    changeDisplay(btnMenuClose, "none");
     hiddenElem(mainMenu);
-    btnMenuOpen.style.visibility = "visible";
+    // btnMenuOpen.style.visibility = "visible";
+    changeDisplay(btnMenuOpen, "block")
   }
 });
 
@@ -25,4 +43,8 @@ function hiddenElem(elem) {
 
 function showElem(elem) {
   elem.classList.remove("visually-hidden");
+}
+
+function changeDisplay(elem, param) {
+    elem.style.display = param;
 }
